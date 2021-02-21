aff865152bbcb6a490cb95608b79a086068a0360
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-02-21 19:30:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return(
+    <>
+      <Layout />
+      <h1> Rocketseat </h1>
+    </>
+  );
+
+
+}
+
+export default App;
