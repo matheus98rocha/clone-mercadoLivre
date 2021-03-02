1ed4bd18326d2b53b6f4e3fdde786e0336b21457
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-03-02 01:13:13 -0300

    Styled Components

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,8 +1,10 @@
+import { info } from 'console';
 import React from 'react';
 
 import tshirtImage from '../../assets/tshirt.png';
 
 import { Container, Row, Panel, Column, Gallery, Section, Description } from './styles';
+
 
 const Product: React.FC = () => {
   return (
@@ -18,6 +20,7 @@ const Product: React.FC = () => {
             <img alt="T-Shirt" src={tshirtImage} />
           </Gallery>
 
+          {/* // eslint-disable-next-line to the line before */}
           <Info />
         </Column>
 
@@ -35,33 +38,16 @@ const Product: React.FC = () => {
   );
 };
 
-const WarrantySection = () => {
-  <Section>
-    <h4>Garantia</h4>
+const Info = () => {
+  return (
+    <Description>
+      <h2>Descrição</h2>
 
-    <div>
-      <span>
-        <p className="title">Compra garantida com o Lorem Ipsum</p>
-        <p className="descriptiom">Receba o produto que deseja ou devolvemos o seu dinheiro</p>
-      </span>
-
-      <span>
-        <p className="title">Garantia do vendedor</p>
-        <p className="descriptiom">Sem garantia</p>
-      </span>
-    </div>
-  </Section>
-}
-
-const info = () => {
-  <Description>
-    <h2>Descrição</h2>
-
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.
       <br />
-      <br />
+        <br />
       Itens inclusos: <br />
       -1x LED <br />
       -1x LED <br />
@@ -73,7 +59,31 @@ const info = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis libero voluptate consectetur sint cupiditate? Ducimus voluptatem asperiores deserunt minima labore quos iusto, tempore, cum maiores perferendis assumenda rerum ad.      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapie
     </p>
 
-  </Description>
+    </Description>
+  )
+}
+
+
+const WarrantySection = () => {
+  return (
+    <Section>
+      <h4>Garantia</h4>
+
+      <div>
+        <span>
+          <p className="title">Compra garantida com o Lorem Ipsum</p>
+          <p className="descriptiom">Receba o produto que deseja ou devolvemos o seu dinheiro</p>
+        </span>
+
+        <span>
+          <p className="title">Garantia do vendedor</p>
+          <p className="descriptiom">Sem garantia</p>
+        </span>
+      </div>
+
+      <a href="#">Saiba mais sobre garantia</a>
+    </Section>
+  )
 }
 
 export default Product;
